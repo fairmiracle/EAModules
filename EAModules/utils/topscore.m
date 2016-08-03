22300599af849e3f852597fa4ca06d0717c13d4e
Dong Li <donggeat@gmail.com>
2016-08-03 19:00:32 +0100

    update

--- EAModules/utils/topscore.m
+++ EAModules/utils/topscore.m
@@ -84,8 +84,8 @@ for i = 1:length(labels)
     nodeList = nodeset(find(Lnew==labels(i)));
     k=length(nodeList);
     compscore = sum(array_basic_z(nodeList))/sqrt(k);
-%    aggregate_score = sum(array_basic_z(nodeList))/sqrt(k);
-%    compscore = (aggregate_score - randomscore(k,1))/randomscore(k,2);
+    %aggregate_score = sum(array_basic_z(nodeList))/sqrt(k);
+    %compscore = (aggregate_score - randomscore(k,1))/randomscore(k,2);
     if compscore > s
         s = compscore;
         subset = nodeList;
